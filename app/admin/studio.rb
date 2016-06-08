2cbf4a9bcac1a72e12ef2127b322df224f66d333
Prelang Builder <builder@prelang.com>
2016-06-08 01:38:48 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Studio' to allow for create, update

--- app/admin/studio.rb
+++ app/admin/studio.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Studio do
+  permit_params :name, :email, :url, :address, :telephone, :introtext, :fulltext, :main_image
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
